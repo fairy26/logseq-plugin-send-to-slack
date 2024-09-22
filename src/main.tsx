ef6b11a67c22382a3dbb827f99d593de7dcc4001
fairy26 <yoshida-yosei-vx@ynu.jp>
2024-09-22 19:35:41 +0900

    delete: template code

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,7 @@ import * as ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-import { logseq as PL } from "../package.json";
-
-// @ts-expect-error
-const css = (t, ...args) => String.raw(t, ...args);
-
-const pluginId = PL.id;
-
 function main() {
-  console.info(`#${pluginId}: MAIN`);
   const root = ReactDOM.createRoot(document.getElementById("app")!);
 
   root.render(
@@ -21,40 +13,6 @@ function main() {
       <App />
     </React.StrictMode>
   );
-
-  function createModel() {
-    return {
-      show() {
-        logseq.showMainUI();
-      },
-    };
-  }
-
-  logseq.provideModel(createModel());
-  logseq.setMainUIInlineStyle({
-    zIndex: 11,
-  });
-
-  const openIconName = "template-plugin-open";
-
-  logseq.provideStyle(css`
-    .${openIconName} {
-      opacity: 0.55;
-      font-size: 20px;
-      margin-top: 4px;
-    }
-
-    .${openIconName}:hover {
-      opacity: 0.9;
-    }
-  `);
-
-  logseq.App.registerUIItem("toolbar", {
-    key: openIconName,
-    template: `
-      <div data-on-click="show" class="${openIconName}">⚙️</div>
-    `,
-  });
 }
 
 logseq.ready(main).catch(console.error);
