bd27de403002cd4ad5b3d02ed00146b804030727
fairy26 <yoshida-yosei-vx@ynu.jp>
2024-09-22 19:46:43 +0900

    feat: add slash command sample

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,20 @@ import * as ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+async function sendToSlack() {
+  const block = await logseq.Editor.getCurrentBlock();
+  if (block === null) {
+    return;
+  }
+
+  const { content, uuid } = block;
+  logseq.UI.showMsg(`
+      [:div.p-2
+      [:h1 "#${uuid}"]
+      [:h2.text-xl "${content}"]]
+    `);
+}
+
 function main() {
   const root = ReactDOM.createRoot(document.getElementById("app")!);
 
@@ -13,6 +27,8 @@ function main() {
       <App />
     </React.StrictMode>
   );
+
+  logseq.Editor.registerSlashCommand("Send to Slack", sendToSlack);
 }
 
 logseq.ready(main).catch(console.error);
